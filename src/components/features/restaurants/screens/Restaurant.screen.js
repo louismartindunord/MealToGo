39c33ad1387926e16a498eaa939473ce132d3f0b
louismartindunord <l.martindunord@gmail.com>
2023-08-31 08:25:54 +0200

    End of styling the flatlistcomponent

--- src/components/features/restaurants/screens/Restaurant.screen.js
+++ src/components/features/restaurants/screens/Restaurant.screen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, SafeAreaView, Text, View } from "react-native";
+import { StatusBar, SafeAreaView, FlatList } from "react-native";
 import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -12,12 +12,29 @@ const SafeArea = styled(SafeAreaView)`
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
+// styled component based on the FlatList, with added styles for content container padding.
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestaurantInfoCard />
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+      ]}
+      renderItem={() => <RestaurantInfoCard />}
+      keyExtractor={(item) => item.name}
+    />
   </SafeArea>
 );
