f438c71cb518604227aa8a33b79a4a0013ab5fa5
louismartindunord <l.martindunord@gmail.com>
2023-09-10 09:24:14 +0200

    petit fic

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { Ionicons } from "@expo/vector-icons";
 
 import { RestaurantsScreen } from "./src/components/features/restaurants/screens/Restaurant.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
@@ -20,7 +21,7 @@ const Map = () => (
   </SafeArea>
 );
 
-const Setting = () => (
+const Settings = () => (
   <SafeArea>
     <Text>Setting!</Text>
   </SafeArea>
@@ -44,10 +45,31 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={RestaurantsScreen} />
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+ 
+                if (route.name === "Restaurants") {
+                  iconName = "ion-ios-home";
+                } else if (route.name === "Settings") {
+                  iconName = "md-settings";
+                } else if (route.name === "Map") {
+                  iconName = "md-map";
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}
+          >
+            <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Setting" component={Setting} />
+            <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
---
+++ src/services/restaurants/mock/restaurants.service.js
@@ -0,0 +1,4 @@
+import { mocks } from "./mock"
+const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+
+}
